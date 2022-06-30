cb7d987e010a4610410a754e46869a5225a4b0c6
이진곤 <qazqww@naver.com>
2022-06-30 14:18:49 +0900

    220630

---
+++ BJAlgorithm/src/BOJ13305_GasStation.java
@@ -0,0 +1,46 @@
+// https://www.acmicpc.net/problem/13305
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ13305_GasStation {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int[] dist = new int[N-1];
+		int[] price = new int[N];
+		long total = 0L;
+		
+		st = new StringTokenizer(in.readLine());
+		for (int i = 0; i < dist.length; i++) {
+			dist[i] = Integer.parseInt(st.nextToken());
+		}
+
+		int nowPrice = -1;
+		long nowDist = 0L;
+		
+		st = new StringTokenizer(in.readLine());
+		for (int i = 0; i < price.length; i++) {
+			price[i] = Integer.parseInt(st.nextToken());
+			
+			if (i == 0)
+				nowPrice = price[i];
+			
+			if (i == price.length - 1 || price[i] < nowPrice) {
+				System.out.println("point : " + i + " / " + nowDist * nowPrice);
+				total += nowDist * nowPrice;
+				nowPrice = price[i];
+				nowDist = 0;
+			}
+
+			if (i != price.length - 1)
+				nowDist += dist[i];
+		}
+		
+		System.out.println(total);
+	}
+}
