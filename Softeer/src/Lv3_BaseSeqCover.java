7930c774dcf5d648c289d124621c8bf908c9a4a1
이진곤 <qazqww@naver.com>
2023-06-16 19:56:54 +0900

    230616 (2)

--- Softeer/src/Lv3_BaseSeqCover.java
+++ Softeer/src/Lv3_BaseSeqCover.java
@@ -1,17 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Lv3_BaseSeqCover {
+    static ArrayList<int[]> canUniteList = new ArrayList<>();
+    static boolean[] tried;
+    static int N, M, min;
+
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(in.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        min = Integer.MAX_VALUE;
         ArrayList<String> seqList = new ArrayList<>();
 
         // 1. 기본 상태에서 합쳐질 수 있는 후보 쌍들을 모두 찾는다.
@@ -22,7 +24,7 @@ public class Lv3_BaseSeqCover {
             seqList.add(in.readLine());
         }
 
-        ArrayList<int[]> canUniteList = new ArrayList<>();
+        canUniteList = new ArrayList<>();
         for (int i = 0; i < N - 1; i++) {
             for (int j = i + 1; j < N; j++) {
                 if (canUnite(seqList.get(i), seqList.get(j))) {
@@ -31,15 +33,53 @@ public class Lv3_BaseSeqCover {
             }
         }
 
-        for (int i = 0; i < canUniteList.size(); i++) {
+        tried = new boolean[canUniteList.size()];
+        unite(N, seqList);
+        System.out.println(min);
+    }
 
+    static void unite(int cnt, ArrayList<String> list) {
+        min = Math.min(min, cnt);
+        for (int i = 0; i < canUniteList.size(); i++) {
+            if (tried[i]) {
+                continue;
+            }
+
+            ArrayList<String> newList = new ArrayList<>();
+            for (String str : list) {
+                newList.add(str);
+            }
+
+            tried[i] = true;
+            int[] target = canUniteList.get(i);
+            if (canUnite(newList.get(target[0]), newList.get(target[1]))) {
+                newList.set(target[0], unite(newList.get(target[0]), newList.get(target[1])));
+                newList.set(target[1], "");
+                unite(cnt - 1, newList);
+            }
+            tried[i] = false;
         }
     }
 
-    static boolean canUnite(String str1, String str2) {
+    static String unite(String str1, String str2) {
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < str1.length(); i++) {
-            if (str1.charAt(i) != '.' && str2.charAt(i) != '.'
-                    && str1.charAt(i) != str1.charAt(i)) {
+            if (str1.charAt(i) == str2.charAt(i) || (str1.charAt(i) != '.' && str2.charAt(i) == '.')) {
+                sb.append(str1.charAt(i));
+            }
+            else if (str1.charAt(i) == '.' && str2.charAt(i) != '.') {
+                sb.append(str2.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    static boolean canUnite(String str1, String str2) {
+        if (str1.equals("") || str2.equals("")) {
+            return false;
+        }
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1.charAt(i) != '.' && str2.charAt(i) != '.' && str1.charAt(i) != str2.charAt(i)) {
                 return false;
             }
         }
